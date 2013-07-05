7a969e930da89fefae40a4fe24ada7faed517454
chihchunchen <chihchun_chen@yahoo.co.uk>
2013-07-05 10:54:57 +0000

    Added buffer stream to web.js

--- web.js
+++ web.js
@@ -5,10 +5,9 @@ app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs = require('fs');
   var buf = fs.readFileSync("index.html");
-  
-print(buf.toString());
   response.send(buf.toString("utf-8"));
 });
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
