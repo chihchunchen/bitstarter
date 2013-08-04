8e405ab68767467dd89246e2a4185315fb64d8ac
chihchunchen <chihchun_chen@yahoo.co.uk>
2013-08-04 09:02:36 +0000

    Reverted web.js

--- web.js
+++ web.js
@@ -2,10 +2,34 @@ var express = require("express");
 var fs = require("fs");
 var htmlfile = "index.html";
 var app = express.createServer(express.logger())
+var path = require("path");
+var url = require("url")
 
 app.get('/', function(request, response) {
-  var html = fs.readFileSync(htmlfile).toString();
-  response.send(html);
+  var uri = url.parse(request.url).pathname, filename = path.join(process.cwd(), uri);
+
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 not found\n");
+      response.end();
+      return;
+    }
+
+    if(fs.statSync(filename).isDirectory()) filename += '/index.html';
+    
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+
+    }
+
+    response.writeHead(200);
+    response.write(file, "binary");
+    response.end(); 
 });
 
 var port = process.env.PORT || 8080;
